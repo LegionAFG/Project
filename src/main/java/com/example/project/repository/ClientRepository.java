2a21790844ac917869ef5fee18052add398cd67c
LegionAFG <badr.said.emil@hotmail.com>
2025-05-21 14:41:28 +0200

    feat(service): HistoriesService mit CRUD-Methoden und @Transactional-Annotation implementieren

--- src/main/java/com/example/project/repository/ClientRepository.java
+++ src/main/java/com/example/project/repository/ClientRepository.java
@@ -4,9 +4,6 @@ import com.example.project.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    List<Client> id(Long id);
 }
--- src/main/java/com/example/project/service/HistoriesService.java
+++ src/main/java/com/example/project/service/HistoriesService.java
@@ -1,5 +1,36 @@
 package com.example.project.service;
 
+import com.example.project.model.Histories;
+import com.example.project.repository.HistoriesRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class HistoriesService {
-    //TODO: implement HistoriesService
+
+    private final HistoriesRepository historiesRepository;
+
+    @Transactional
+    public void saveHistories(Histories histories) {
+        log.info("Historie gespeichert {}", histories);
+        historiesRepository.save(histories);
+    }
+
+    @Transactional(readOnly = true)
+    public Histories getHistoriesById(Long id) {
+        return historiesRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Historie mit nicht gefunden"));
+    }
+
+    @Transactional
+    public void deleteHistorie(Long id){
+        if(!historiesRepository.existsById(id))
+            throw new IllegalArgumentException("Historie mit ID " + id + " nicht gefunden");
+        log.info("Historie gelöscht mit ID : {}", id);
+        historiesRepository.deleteById(id);
+    }
 }
